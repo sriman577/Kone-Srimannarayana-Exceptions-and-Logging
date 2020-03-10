4170c6daf70c440b5275d599f3bd3e536cf75025
sriman577 <17pa1a0577@vishnu.edu.in>
2020-03-10 17:16:01 +0530

    first commit

---
+++ Exceptions_Logging/src/main/java/com/sriman/Exceptions_Logging/Estimation.java
@@ -0,0 +1,22 @@
+package com.sriman.Exceptions_Logging;
+
+
+public class Estimation {
+	public double estimateHouse(double area,int standard,boolean fullyautomated){
+        int arr[] = new int[4];
+        arr[0] = 1200;
+        arr[1] = 1500;
+        arr[2] = 1800;
+        arr[3] = 2500;
+        int costpersquarefeet = 0;
+        double constructionCost = 0;
+        if(fullyautomated){
+            costpersquarefeet = arr[3];
+        }
+        else {
+            costpersquarefeet = arr[standard-1];
+        }
+        constructionCost = area*costpersquarefeet;
+        return constructionCost;
+    }
+}
